105ebf79ac0ad8d19ff7fc692eaeeebd6f1a888d
dvegaa20 <diego.vega.camacho@gmail.com>
2025-03-13 13:41:37 -0600

    feat(main_page): restructure homepage components and improve layout

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,16 +1,24 @@
 import Hero from "@/components/sections/Hero";
-import Services from "@/components/sections/Services";
+import Features from "@/components/sections/Features";
+import Showcase from "@/components/sections/Showcase";
+import Stats from "@/components/sections/Stats";
 import Testimonials from "@/components/sections/Testimonials";
 import Pricing from "@/components/sections/Pricing";
+import Clients from "@/components/sections/Clients";
+import Cta from "@/components/sections/Cta";
 import Contact from "@/components/sections/Contact";
 
 export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center">
+    <main className="flex min-h-screen flex-col items-center w-full overflow-hidden">
       <Hero />
-      <Services />
+      <Clients />
+      <Features />
+      <Showcase />
+      <Stats />
       <Testimonials />
       <Pricing />
+      <Cta />
       <Contact />
     </main>
   );
